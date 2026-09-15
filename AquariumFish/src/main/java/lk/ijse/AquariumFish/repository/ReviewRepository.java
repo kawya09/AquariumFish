54dcb7b389c623e405bd5013d8b6b37f1a18e340
kawya09 <h7346917@gmail.com>
2026-09-15 13:00:02 +0530

    it

--- AquariumFish/src/main/java/lk/ijse/AquariumFish/repository/ReviewRepository.java
+++ AquariumFish/src/main/java/lk/ijse/AquariumFish/repository/ReviewRepository.java
@@ -2,9 +2,14 @@ package lk.ijse.AquariumFish.repository;
 
 import lk.ijse.AquariumFish.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query(value = "SELECT * FROM review WHERE ?1 IS NULL OR comment LIKE CONCAT('%', ?1, '%')",
+            nativeQuery = true)
     List<Review> findByCommentContaining(String comment);
+
 }
